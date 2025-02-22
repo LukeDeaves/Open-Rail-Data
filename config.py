fe0305d47c72549314f9f047a02e9c14ec8aaed7
MightyOwl <lukedeaves@yahoo.co.uk>
2025-02-22 21:19:03 +0000

    Update config directory comment

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ APP_VERSION = "1.0.0"
 APP_PUBLISHER = "Luke Deaves"
 APP_NAME = "National Rail Data Downloader"
 APP_SERIAL = f"{APP_NAME} {APP_VERSION}"
-CONFIG_DIR = os.path.join('~/Documents', APP_PUBLISHER, APP_NAME) # To use appdata instead of documents, os.getenv('APPDATA')
+CONFIG_DIR = os.path.join('~/Documents', APP_PUBLISHER, APP_NAME) # os.getenv('APPDATA')
